baf6de965f52b05f8f55315d09dc959486643198
hiroshi498 <107475017+hiroshi498@users.noreply.github.com>
2022-10-13 15:44:25 +0900

    料金表以外の完成

--- javascript/style.js
+++ javascript/style.js
@@ -3,3 +3,10 @@ $(function() {
     $('body').toggleClass('is-open');
   });
 });
+
+new ScrollHint('.js-scrollable', {
+  scrollHintIconAppendClass: 'scroll-hint-icon-white', // white-icon will appear
+  i18n: {
+    scrollable: 'スクロールできます'
+  }
+});
